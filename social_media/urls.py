84775dd53e079c989a2e7378a98c977bb39142c7
parabavire <jdparabavire@gmail.com>
2025-08-06 00:01:54 -0400

    creación de posts hecho

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,11 +1,26 @@
+#from django.contrib import admin
+#from django.urls import path, include #JD 05 08
+#from core.views import index, signup_view, login_view, logout_view
+#
+#urlpatterns = [
+#    path("admin/", admin.site.urls),
+#    path("", index, name="index"),
+#    path("signup/", signup_view, name="signup"),
+#    path("login/", login_view, name="login"),
+#    path("logout/", logout_view, name="logout"),
+#    path('' , include('core.urls')),  #JD 05 08
+#]
+
+
+# social_media/urls.py
+
+#--------------------------------------------
+# sistema de publicaciones (post)  #JD 05 08
 from django.contrib import admin
-from django.urls import path
-from core.views import index, signup_view, login_view, logout_view
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", index, name="index"),
-    path("signup/", signup_view, name="signup"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
+#--------------------------------------------
